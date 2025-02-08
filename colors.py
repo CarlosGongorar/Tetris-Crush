37d07eccc73852cf56d2d6ebc04d5caa1d77e5da
CarlosGongorar <carlosgongorar50@gmail.com>
2025-02-07 20:33:54 -0500

    Fixing Orange Color

--- colors.py
+++ colors.py
@@ -2,7 +2,7 @@ class Colors:
     dark_grey = (26, 31, 40);
     green = (47, 230, 23);
     red = (232, 18, 18);
-    orange = (226, 18, 18);
+    orange = (255, 98, 8);
     yellow = (237, 234, 4);
     purple = (166, 0, 247);
     cyan = (21, 204, 209);
