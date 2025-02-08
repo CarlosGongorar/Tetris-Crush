6525ed68d74d31d8ea5217ef3ba0cb485e8f2572
CarlosGongorar <carlosgongorar50@gmail.com>
2025-02-07 19:48:18 -0500

    Cheaking completed rows

--- game.py
+++ game.py
@@ -44,6 +44,7 @@ class Game:
             self.grid.grid[pos.row][pos.column] = self.current_block.id
         self.current_block = self.next_block
         self.next_block = self.get_random_block();
+        self.grid.clear_full_rows();
     
     def block_fits(self):
         tiles = self.current_block.get_cell_positions();
